af3630cd401789df1db390dfb18322351bdcda9d
Chlo06 <fernandez.chloe06@gmail.com>
2020-12-02 13:10:40 +0100

    modify commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-class vehicule{
+class voiture {
 
-    constructor(immat, carColor, weight, power, tank, places){
+    constructor(license, Color, weight, power, tank, places){
 
-    this.immat = immat;
-    this.carColor = carColor;
+    this.license = license;
+    this.Color = Color;
     this.weight = weight;
     this.power = power;
     this.tank = tank;
@@ -11,14 +11,36 @@ class vehicule{
     }  
 
 repeindre (newColor) {
-if (newColor === this.carColor){
+   if (newColor === this.Color){
     console.log ('Thanks for the refreshment')
 }  else{
-    this.carColor = newColor;
+    this.Color = newColor;
     console.log (newColor + ' ' + 'Great choice thanks')
-      }
+    }
+   }
+
+mettreEssence (addGas) {
+    if ((addGas + gas) <= this.tank){
+     gas += addGas
+     console.log('Your tank containt now ' + gas);
+}   else{
+    console.log(' Not possible too much gas add');
+     }
+ }
+
+seDeplacer(distance , speed) {
+
+}
+toString (aboutCar){
+    return`About youre car : license is ${this.license}, power is ${this.power} and color is ${this.Color}`
 }
 }
-let car1 = new vehicule ("CF501MM" , "pink" , 1500 , 110 , 10.0 , 5)
-console.log(car1)
+let car1 = new voiture ("CF501MM" , "pink" , 1500 , 110 , 60.0 , 5)
+let gas =(5.0)
+
+//console.log(car1)
 car1.repeindre("green")
+car1.mettreEssence (150)
+//console.log(car1)
+
+console.log (car1.toString());
