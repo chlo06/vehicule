a836b7ab86262f43b977788eca1e84899825347d
Chlo06 <fernandez.chloe06@gmail.com>
2020-12-02 14:10:14 +0100

    total code

--- main.js
+++ main.js
@@ -24,23 +24,44 @@ mettreEssence (addGas) {
      gas += addGas
      console.log('Your tank containt now ' + gas);
 }   else{
-    console.log(' Not possible too much gas add');
+    console.log('Not possible too much gas add');
      }
  }
 
 seDeplacer(distance , speed) {
+    if (speed < 50){
+       gasTrip = ((distance *10)/100);
+}
+   if ((speed >= 50) && (speed < 90 )){
+       gasTrip= ((distance * 5)/100);
+}
+   if ((speed >= 90) && (speed <130)){
+       gasTrip= ((distance * 8)/100);
+}
+   if (speed >=130){
+       gasTrip= ((distance * 12)/100);
+   }
+   if (gas >= gasTrip){
+       console.log('You can do this'+ ' ' + gasTrip +'L' + ' '+  'be consumed for this trip ');
+   } else {
+       console.log('No possible gas too low ');
+   }
+}
 
-}
-toString (aboutCar){
+
+toString (){
     return`About youre car : license is ${this.license}, power is ${this.power} and color is ${this.Color}`
+    }
 }
-}
+
+
 let car1 = new voiture ("CF501MM" , "pink" , 1500 , 110 , 60.0 , 5)
 let gas =(5.0)
-
+let gasTrip = null;
 //console.log(car1)
 car1.repeindre("green")
 car1.mettreEssence (150)
+car1.seDeplacer ( 10, 4)
 //console.log(car1)
 
-console.log (car1.toString());
+//console.log (car1.toString());
