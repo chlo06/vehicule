5c3187f16f425c4beae19920629d8a96d13ae75b
Chlo06 <fernandez.chloe06@gmail.com>
2020-12-03 13:07:07 +0100

    Final commit

--- Voiture.js
+++ Voiture.js
@@ -1,30 +1,32 @@
-class Voiture {
+class Voiture{
 
     constructor(license, color, weight, power, tank, places,){
 
-    this.license = license;
-    this.color = color;
-    this.weight = weight;
-    this.power = power;
-    this.tank = tank;
-    this.places = places;
-    this.gas = 5;
-    this.insurance = false ;
+    this.license = license
+    this.color = color
+    this.weight = weight
+    this.power = power
+    this.tank = tank
+    this.places = places
+    this.gas = 5
+    this.insurance = false 
     this.boardMessage = 'You dont have any insurance cannot take the road like that'    
 }
 addInsurance (newInsurance){
     if (newInsurance === this.insurance) {
         this.boardMessage = 'You dont have any insurance cannot take the road like that'
+        return this.boardMessage
     } else {
         this.insurance = newInsurance
         this.boardMessage = 'Great you have now an insurance ! Enjoy'
+        return this.boardMessage
         }
     }
 repeindre (newColor) {
    if (newColor == this.color){
     return 'Thanks for the refreshment'
 }  else{
-    this.color = newColor;
+    this.color = newColor
      return this.color + ' ' + 'Great choice thanks'
     }
    }
@@ -32,29 +34,29 @@ repeindre (newColor) {
 mettreEssence (addGas) {
     if ((addGas + this.gas) <= this.tank){
      this.gas += addGas
-     return 'Your tank containt now ' + this.gas;
-    } return 'Not possible too much gas add';
+     return 'Your tank containt now ' + this.gas
+    } return 'Not possible too much gas add'
      }
  
 
 seDeplacer(distance , speed) {
-    this.gasTrip = null;
+    this.gasTrip = null
 
     if (speed < 50){
-     this.gasTrip = ((distance *10)/100);
+     this.gasTrip = ((distance *10)/100)
 }
    if ((speed >= 50) && (speed < 90 )){
-     this.gasTrip= ((distance * 5)/100);
+     this.gasTrip= ((distance * 5)/100)
 }
    if ((speed >= 90) && (speed <130)){
-     this.gasTrip= ((distance * 8)/100);
+     this.gasTrip= ((distance * 8)/100)
 }
    if (speed >=130){
     this.gasTrip= ((distance * 12)/100)
-   } ;
+   } 
    
    if (this.gas >= this.gasTrip){
-       return 'You can do this'+ ' ' + this.gasTrip +'L' + ' '+  'be consumed for this trip ';
+       return 'You can do this'+ ' ' + this.gasTrip +'L' + ' '+  'be consumed for this trip '
    }   return 'No possible gas too low ';
   }
 
