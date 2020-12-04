6e2b0d1cab6498bf0eb48662ee5a9b86891c6f39
Chlo06 <fernandez.chloe06@gmail.com>
2020-12-04 12:28:11 +0100

    Final modify

--- Voiture.js
+++ Voiture.js
@@ -1,66 +1,69 @@
-class Voiture{
-
-    constructor(license, color, weight, power, tank, places,){
-
-    this.license = license
-    this.color = color
-    this.weight = weight
-    this.power = power
-    this.tank = tank
-    this.places = places
-    this.gas = 5
-    this.insurance = false 
-    this.boardMessage = 'You dont have any insurance cannot take the road like that'    
-}
-addInsurance (newInsurance){
+class Voiture {
+  constructor(license, color, weight, power, tank, places) {
+    this.license = license;
+    this.color = color;
+    this.weight = weight;
+    this.power = power;
+    this.tank = tank;
+    this.places = places;
+    this.gas = 5;
+    this.insurance = false;
+    this.boardMessage =
+      "You dont have any insurance cannot take the road like that";
+  }
+  addInsurance(newInsurance) {
     if (newInsurance === this.insurance) {
-        return this.boardMessage
+      return this.boardMessage;
     } else {
-        this.insurance = newInsurance
-        this.boardMessage = 'Great you have now an insurance ! Enjoy'
-        return this.boardMessage
-        }
+      this.insurance = newInsurance;
+      this.boardMessage = "Great you have now an insurance ! Enjoy";
+      return this.boardMessage;
     }
-repeindre (newColor) {
-   if (newColor == this.color){
-    return 'Thanks for the refreshment'
-}  else{
-    this.color = newColor
-     return this.color + ' ' + 'Great choice thanks'
+  }
+  repeindre(newColor) {
+    if (newColor == this.color) {
+      return "Thanks for the refreshment";
+    } else {
+      this.color = newColor;
+      return this.color + " " + "Great choice thanks";
     }
-   }
-
-mettreEssence (addGas) {
-    if ((addGas + this.gas) <= this.tank){
-     this.gas += addGas
-     return 'Your tank containt now ' + this.gas
-    } return 'Not possible too much gas add'
-     }
- 
-
-seDeplacer(distance , speed) {
-    this.gasTrip = null
-
-    if (speed < 50){
-     this.gasTrip = ((distance *10)/100)
-}
-   if ((speed >= 50) && (speed < 90 )){
-     this.gasTrip= ((distance * 5)/100)
-}
-   if ((speed >= 90) && (speed <130)){
-     this.gasTrip= ((distance * 8)/100)
-}
-   if (speed >=130){
-    this.gasTrip= ((distance * 12)/100)
-   } 
-   
-   if (this.gas >= this.gasTrip){
-       return 'You can do this'+ ' ' + this.gasTrip +'L' + ' '+  'be consumed for this trip '
-   }   return 'No possible gas too low ';
   }
 
-toString (){
-    return`About youre car : license is ${this.license}, power is ${this.power} and color is ${this.color}`
+  mettreEssence(addGas) {
+    if (addGas + this.gas <= this.tank) {
+      this.gas += addGas;
+      return "Your tank containt now " + this.gas;
     }
+    return "Not possible too much gas add";
+  }
+
+  seDeplacer(distance, speed) {
+    this.gasTrip = null;
+
+    if (speed < 50) {
+      this.gasTrip = (distance * 10) / 100;
+    } else if (speed >= 50 && speed < 90) {
+      this.gasTrip = (distance * 5) / 100;
+    } else if (speed >= 90 && speed < 130) {
+      this.gasTrip = (distance * 8) / 100;
+    } else if (speed >= 130) {
+      this.gasTrip = (distance * 12) / 100;
+    }
+
+    if (this.gas >= this.gasTrip) {
+      return (
+        "You can do this" +
+        " " +
+        this.gasTrip +
+        "L" +
+        " " +
+        "be consumed for this trip "
+      );
+    }
+    return "No possible gas too low ";
+  }
+
+  toString() {
+    return `About youre car : license is ${this.license}, power is ${this.power} and color is ${this.color}`;
+  }
 }
-    
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let mercedes = new Voiture ("CF-501-MM" , "pink" , 1500 , 110 , 60.0 , 5 )
+const mercedes = new Voiture("CF-501-MM", "pink", 1500, 110, 60.0, 5);
 
-console.log(mercedes.insurance)
-console.log(mercedes.boardMessage)
+console.log(mercedes.insurance);
+console.log(mercedes.boardMessage);
 
-console.log(mercedes.repeindre('red'))
-console.log(mercedes.mettreEssence(30))
-console.log(mercedes.seDeplacer (10, 4))
-console.log(mercedes.addInsurance(true))
+console.log(mercedes.repeindre("red"));
+console.log(mercedes.mettreEssence(30));
+console.log(mercedes.seDeplacer(10, 4));
+console.log(mercedes.addInsurance(true));
 
-console.log(mercedes)
-console.log(mercedes.toString)
+console.log(mercedes);
+console.log(mercedes.toString());
